cedb6e079188d7827287cd5d7f8c2d52258d6e21
TuanLamNguyen <tuanlamnguyen99@gmail.com>
2024-05-16 16:35:07 +0700

    Update sql_practice3.sql

--- sql_practice3.sql
+++ sql_practice3.sql
@@ -27,6 +27,21 @@ group by extract(month from submit_date), product_id
 order by mth,product_id
 
 --ex5
+SELECT sender_id, 
+count(content) as message_count
+FROM messages
+WHERE extract(month from sent_date)=8 AND 
+extract(year from sent_date) = 2022
+group by sender_id
+order by message_count DESC
+limit 2
+
+--ex6
+select tweet_id
+from Tweets
+where length(content) >15
+
+--ex7
 
 
 
